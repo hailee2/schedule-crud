b34b26db1ef9de449ad7edc6fca170dc9d627f7b
hailee2 <slowslow9150@gmail.com>
2025-09-02 17:31:58 +0900

    [4단계] 사용하지않는 임포트 삭제

--- src/main/java/example/demo/user/controller/UserController.java
+++ src/main/java/example/demo/user/controller/UserController.java
@@ -4,7 +4,6 @@ import example.demo.common.consts.Const;
 import example.demo.user.dto.*;
 import example.demo.user.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
